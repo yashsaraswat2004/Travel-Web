24c01cea508d431ae0b537e48dacce87830eda8d
Yashsaraswat2004 <saraswatyash2004@gmail.com>
2024-10-08 12:10:03 +0530

    Added Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="bg-slate-800 text-3xl text-black">
+        Travel Website Project
+      </h1>
+    </div>
+  );
+}
+
+export default App;
