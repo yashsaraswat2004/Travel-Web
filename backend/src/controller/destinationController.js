d716a391013f0d3893eca508f30051d731a2df2e
Yashsaraswat2004 <saraswatyash2004@gmail.com>
2024-10-24 15:57:26 +0530

    now  you can search by pressing enter key

--- backend/src/controller/destinationController.js
+++ backend/src/controller/destinationController.js
@@ -1,47 +1,49 @@
 import Destination from "../models/destinationModel.js";
 
 const searchDestinations = async (req, res) => {
-    try {
-        const { keyword } = req.query;
+  try {
+    const { keyword } = req.query;
 
-        const destinations = await Destination.find({
-            $or: [
-                { name: { $regex: keyword, $options: 'i' } },
-                { city: { $regex: keyword, $options: 'i' } },
-                { country: { $regex: keyword, $options: 'i' } }
-            ]
-        });
+    const destinations = await Destination.find({
+      $or: [
+        { name: { $regex: keyword, $options: "i" } },
+        { city: { $regex: keyword, $options: "i" } },
+        { country: { $regex: keyword, $options: "i" } },
+      ],
+    });
 
-        if (destinations.length === 0)
-            return res.status(404).json({ message: "No destinations found" });
+    if (destinations.length === 0)
+      return res.status(404).json({ message: "No destinations found" });
 
-        return res.status(200).send(destinations)
-    } catch (error) {
-        return res.status(500).json({ message: "error in finding destinations" });
-    }
-}
+    return res.status(200).send(destinations);
+  } catch (error) {
+    return res.status(500).json({ message: "error in finding destinations" });
+  }
+};
 
 const getDestinationById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const destination = await Destination.findById(id);
+  try {
+    const { id } = req.params;
+    const destination = await Destination.findById(id);
 
-        if (!destination)
-            return res.status(404).json({ message: `No destinations found with id ${id} ` });
+    if (!destination)
+      return res
+        .status(404)
+        .json({ message: `No destinations found with id ${id} ` });
 
-        return res.status(200).json(destination);
-    } catch (error) {
-        return res.status(500).json({ message: "error in finding destinations" });
-    }
-}
+    return res.status(200).json(destination);
+  } catch (error) {
+    return res.status(500).json({ message: "error in finding destinations" });
+  }
+};
 
 const getAllDestinations = async (req, res) => {
-    try {
-        const destinatons = await Destination.find();
-        return res.status(200).json(destinatons);
-    } catch (error) {
-        return res.status(500).json({ message: "error in finding destinations" });
-    }
-}
+  try {
+    const destinatons = await Destination.find();
+    return res.status(200).json(destinatons);
+  } catch (error) {
+    return res.status(500).json({ message: "error in finding destinations" });
+  }
+};
 
-export { searchDestinations, getDestinationById, getAllDestinations };  
+export { searchDestinations, getDestinationById, getAllDestinations };
